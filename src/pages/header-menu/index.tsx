4d4640f90c96fb7c50bc7f137a8324e71779ca5b
CCZX <ccdebuging@gmail.com>
2021-01-24 01:18:40 +0800

    feat: 增加头部

--- src/pages/header-menu/index.tsx
+++ src/pages/header-menu/index.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from 'react'
+import logo from './../../assets/logo.png'
+import './index.scss'
 
 const HeaderMenu: FC<any> = (props) => {
   return <div className="header-menu">
-    header-menu
+    <div className="header-menu__logo">
+      <img src={logo} height="100%" alt=""/>
+      <span className="title">Page Generate</span>
+    </div>
   </div>
 }
 
